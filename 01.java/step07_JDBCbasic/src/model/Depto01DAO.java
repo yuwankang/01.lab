160b3055220a40956b7980e53815d4c49ab01368
yuwan kang <kangyuwan@naver.com>
2024-07-29 17:53:28 +0900

    20240729 오후 공부

--- 01.java/step07_JDBCbasic/src/model/Depto01DAO.java
+++ 01.java/step07_JDBCbasic/src/model/Depto01DAO.java
@@ -1,13 +1,8 @@
 /* DAO 클래스
- * 	- dept01 table과 매핑되는 db 연동 클래스
- * PreparedStatement API 특징
- * - DB에 요청이 되면 동적 방식
- * 	- 문장 받고 -> 문법 검증 -> 실행
- * 	- 매순간 요청시 마다 : 검증 작업 수행 - Statement
- * 	- 최초 받고 매 요청시 재사용 : PreparedStatement(문법 검증 생략)
+ * - dept01 table과 매핑되는 db 연동 클래스
  * 
- * - JPA & Mybatis라는 db연동 framework들의 기본 API로 내부적으로 사용
  */
+
 package model;
 
 import java.sql.Connection;
@@ -21,49 +16,86 @@ import org.junit.Test;
 import db.util.DBUtil;
 import model.domain.Dept01DTO;
 
-public class Dept01DAO {
-	//모든 부서 정보 검색
+public class Depto01DAO {
+
+	// 모든 부서 정보 검색
+	/*
+	 * PreparedStatement API특징 - db에 요청이 되면 동작방식 문장 받고 -> 문법 검증 -> 실행 매 순간 요청시 마다:
+	 * 검증 작업 수행 - Statement 최초 받고 매 용청시 재사용 - PreparedStatement(문법 검증 생략)
+	 * 
+	 * - JPA & Mybatis라는 db연동 framework등의 기본 API로 내부적으로 사용
+	 */
+
+
+	
+	
 	@Test
-	public void junitTest() {
+	public void juniTest() {
 		try {
-			System.out.println(getDept01All());
+//			System.out.println(getDept01ALL());
+			System.out.println(getDept01One(10));
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	public ArrayList<Dept01DTO> getDept01All() throws SQLException{
+	
+	
+	public ArrayList<Dept01DTO> getDept01ALL() throws SQLException {
 		Connection con = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		ArrayList<Dept01DTO> datas = null;
 		
+		ArrayList<Dept01DTO> datas = null;
+
 		try {
 			con = DBUtil.getConnection();
 			pstmt = con.prepareStatement("select * from dept01");
 			rs = pstmt.executeQuery();
 			
 			datas = new ArrayList<>();
-			
-			while(rs.next()) {//반복 횟수가 불 명확할 때 사용하는 반복문
-				datas.add(new Dept01DTO(rs.getInt(1),
-										rs.getString("dname"), 
-										rs.getString(3)) );
+			while (rs.next()) { // 반복 획수가 불명확 할때 주로 사용되는 반복문
+				datas.add(new Dept01DTO(rs.getInt(1), rs.getString("dname"), rs.getString(3)));
 			}
-		}finally {
+		} finally {
 			DBUtil.close(con, pstmt, rs);
 		}
-		
 		return datas;
 	}
-	//하나의 부서 번호로 검색
-	/* 필요 데이터 : deptno
-	 * 반환값은 : DeptDTO
+
+	// 하나의 부서 번호로 검색
+	/*
+	 * 필요 데이터: deptno 반환값: DeptDTO
+	 * client -> view: 데이터 입력 -> Controller: 입력 여부 검증
+	 * -> controller: 입력 여부 검증 -> 입력이 되었을 경우만 실행되게 작업
 	 */
-	public Dept01DTO getDept01One(int deptno) {
-		return null;
+	public Dept01DTO getDept01One(int deptno) throws SQLException {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		Dept01DTO datas = null;
+		
+		
+		try {
+			con = DBUtil.getConnection();
+			pstmt = con.prepareStatement("select * from dept01 WHERE deptno = ?");
+			pstmt.setInt(1, deptno);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) { // 반복 획수가 불명확 할때 주로 사용되는 반복문
+				datas=new Dept01DTO(rs.getInt(1), rs.getString("dname"), rs.getString(3));
+			}
+		
+		} finally {
+			DBUtil.close(con, pstmt, rs);
+		}
+		return datas;
 	}
 	
-	//새로운 부서 정보 저장
+	
+	
+
+	// 새로운 부서 정보 저장
 	public boolean saveDept01(Dept01DTO oneDept) {
 		return false;
 	}
--- 01.java/step08_gradle/src/main/java/edu/ce/fisa/Step08GradleApplication.java
+++ 01.java/step08_gradle/src/main/java/edu/ce/fisa/Step08GradleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Step08JdbcBasicApplication {
+public class Step08GradleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Step08JdbcBasicApplication.class, args);
+		SpringApplication.run(Step08GradleApplication.class, args);
 	}
 
 }
