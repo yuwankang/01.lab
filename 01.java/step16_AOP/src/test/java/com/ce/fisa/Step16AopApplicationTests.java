38cc1ed0aa14a9ee5b76a54f740050ace5505948
yuwankang <kangyuwan@naver.com>
2024-08-12 10:02:34 +0900

    20240812 Aop 세팅

---
+++ 01.java/step16_AOP/src/test/java/com/ce/fisa/Step16AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ce.fisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Step16AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
